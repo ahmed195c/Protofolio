a6dbbf759a39b31639d6678bd8aecba6f5c62fa9
ahmedjhin <a7madti90@hotmail.com>
2024-05-21 23:38:21 +0400

    po

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function smoothScrollTo(targetId) {
-  const targetElement = document.getElementById(targetId);
-  if (targetElement) {
-    window.scrollTo({
-      top: targetElement.offsetTop,
-      behavior: 'smooth' // Smooth scrolling behavior
-    });
-  }
-}
 
-smoothScrollTo('#top')
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <button onclick={ () => setRuns( labs + 1 ) } >button</button>
-      </div>
       <div>
         <ul className='flex gap-5'>
           <li>
