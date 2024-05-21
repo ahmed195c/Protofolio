8643e0e1da04d25863c0afd285915c9969a226f0
ahmedjhin <a7madti90@hotmail.com>
2024-05-21 22:15:57 +0400

    la

--- src/bottom.jsx
+++ src/bottom.jsx
@@ -4,8 +4,8 @@ function Bottom() {
       <>
         <div className="pt-[2000px]">
             <div id="top" className="pt-[1000px] section">
-                <h1>Bootm tersddddddddddddddt</h1>d
-            </div>
+                <h1>Bootm tersddddddddddddddt</h1>dewqe
+            </div>ewqe
         </div>
       </>
     )
